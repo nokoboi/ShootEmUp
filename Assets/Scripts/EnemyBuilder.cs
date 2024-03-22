d46fde660b5fb7faedab60b34f43fbb9487d2ddd
Kevin <msnrelleno1@hotmail.com>
2024-03-22 10:11:14 +0100

    añadiendo constructor del enemigo

---
+++ Assets/Scripts/EnemyBuilder.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Splines;
+
+namespace Shmup
+{
+    public class EnemyBuilder
+    {
+        GameObject enemyPrefab;
+        SplineContainer spline;
+        GameObject weaponPrefab;
+        float speed;
+
+        public EnemyBuilder SetBasePrefab(GameObject prefab)
+        {
+            enemyPrefab = enemyPrefab; ;
+            return this;
+        }
+
+        public EnemyBuilder SetSpline(SplineContainer spline)
+        {
+            this.spline = spline;
+            return this;
+        }
+
+        public EnemyBuilder SetSpeed(float speed)
+        {
+            this.speed = speed;
+            return this;
+        }
+
+        public GameObject Build()
+        {
+            GameObject instance = GameObject.Instantiate(enemyPrefab);
+
+            SplineAnimate splineAnimate = instance.GetComponent<SplineAnimate>();
+            splineAnimate.Container = spline;
+            splineAnimate.AnimationMethod = SplineAnimate.Method.Speed;
+            splineAnimate.ObjectUpAxis = SplineAnimate.AlignAxis.ZAxis;
+            splineAnimate.ObjectForwardAxis = SplineAnimate.AlignAxis.YAxis;
+            splineAnimate.MaxSpeed = speed;
+
+            return instance;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyType.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Shmup
+{
+    [CreateAssetMenu(fileName = "EnemyType", menuName = "Shmup/EnemyType", order = 0)]
+    public class EnemyType : ScriptableObject
+    {
+        public GameObject enemyPrefab;
+        public GameObject weapoPrefab;
+        public float speed;
+    }
+}
