32c2180ff2ed521a8dd32b8da03c245bee35412a
Kevin <kevin.sanchez@civica-soft.com>
2024-04-21 23:06:20 +0200

    añadido menu seleccion $11c

--- Assets/Scripts/Menu/InsertarMoneda.cs
+++ Assets/Scripts/Menu/InsertarMoneda.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Shmup
 {
@@ -9,14 +10,26 @@ namespace Shmup
     {
         public TextMeshProUGUI textoMoneda1;
         public TextMeshProUGUI textoMoneda2;
+        public GameObject menuSeleccion;
+
+        public bool limiteMonedas;
         private void Update()
         {
             // Detectar si se presiona la barra espaciadora
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (!limiteMonedas && Input.GetKeyDown(KeyCode.Space))
             {
 
-                // Llamar al método AgregarMonedas del GameManager para agregar una moneda
-                GameManager.instance.AgregarMonedas(1);
+                if(GameManager.instance.monedas < 3)
+                {
+                    // Llamar al método AgregarMonedas del GameManager para agregar una moneda
+                    GameManager.instance.AgregarMonedas(1);
+                }
+
+                // Para que pare una vez tenga 3 monedas y aunque siga pulsando espacio no siga sumando monedas
+                if (GameManager.instance.monedas >= 3)
+                {
+                    limiteMonedas = true;
+                }
 
             }
             if (GameManager.instance.monedas > 0)
@@ -26,6 +39,19 @@ namespace Shmup
                 textoMoneda1.text = "Tienes " + GameManager.instance.monedas + " monedas. Pulsa cualquier boton para continuar.";
                 textoMoneda2.text = "Tienes " + GameManager.instance.monedas + " monedas. Pulsa cualquier boton para continuar.";
             }
+
+            if (GameManager.instance.monedas >= 3)
+            {
+                textoMoneda1.gameObject.SetActive(false);
+                textoMoneda2.gameObject.SetActive(false);
+
+                menuSeleccion.SetActive(true);
+            }
+        }
+
+        public void UnJugador()
+        {
+            SceneManager.LoadScene("Level1");
         }
     }
 }
